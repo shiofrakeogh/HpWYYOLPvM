20e660bbba20bf861597fa5b415ab2b45656df9e
Shiofra Keogh <shiofra.keogh@aciworldwide.com>
2026-07-03 19:11:35 +0100

    Adding test for status codes

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,3 +4,8 @@ def test_missing_message():
     client = app.test_client()
     response = client.post("/movies", json={})
     assert response.status_code == 400
+
+def test_empty_body_returns_400():
+    client = app.test_client()
+    response = client.post("/movies", content_type="application/json", data="not json")
+    assert response.status_code == 400
